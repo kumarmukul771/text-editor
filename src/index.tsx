194727682159258b9182e4ae3b03dd77eeb60684
kumarmukul771 <kumarmukul771@gmail.com>
2021-03-28 01:03:31 +0530

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import ReactDOM from 'react-dom';
+import TextEditor from './components/text-editor';
+
+const App = () => {
+  return (
+    <div>
+      <TextEditor />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
